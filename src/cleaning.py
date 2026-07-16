61b01e3770d7de5ecca36a753733229776f90307
Sanju Gawdiya <mrsanju79016@gmail.com>
2026-07-17 02:29:58 +0530

    Inspecting Dataset

--- src/cleaning.py
+++ src/cleaning.py
@@ -5,3 +5,28 @@ df = pd.read_excel("data/raw/Dataset for Data Analytics.xlsx")
 
 # Display first 5 rows
 print(df.head())
+
+# Check dataset informaion
+df.info()
+
+# Null Count
+print(df.isna().sum())
+
+# Duplicate Count
+print("Duplicate Count : ", df.duplicated().sum())
+
+# Checking Unique Values for Categorical Columns
+categorical_columns = [
+    "Product",
+    "PaymentMethod",
+    "OrderStatus",
+    "CouponCode",
+    "ReferralSource"
+]
+
+for col in categorical_columns:
+    print(f"\nUnique values in {col}:")
+    print(df[col].unique())
+
+# Check Numerical Columns
+print(df.describe())
